b68429bd3954f40979173e5d22c3493c2cc1a487
lotosbin <lotosbin@gmail.com>
2025-10-22 22:57:54 +0800

    build: 优化发布插件配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,44 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.publish) apply false
+}
+subprojects {
+    group = "com.yuanjingtech.boot.app.kmp"
+    version = "0.0.2"
+    plugins.withId("com.vanniktech.maven.publish") {
+        configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+            publishToMavenCentral()
+
+            signAllPublications()
+
+            coordinates(group.toString(), "shared", version.toString())
+
+            pom {
+                name = "yuanjingtech's boot library for kmp app"
+                description = "yuanjingtech's boot library for kmp app."
+                inceptionYear = "2025"
+                url = "https://github.com/yuanjingtech/boot-app-kmp"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://github.com/yuanjingtech/boot-app-kmp/blob/main/LICENSE"
+                        distribution = "https://github.com/yuanjingtech/boot-app-kmp/blob/main/LICENSE"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "yuanjingtech"
+                        name = "yuanjingtech"
+                        url = "https://github.com/yuanjingtech"
+                    }
+                }
+                scm {
+                    url = "https://github.com/yuanjingtech/boot-app-kmp"
+                    connection = "scm:git:git://github.com/yuanjingtech/boot-app-kmp.git"
+                    developerConnection = "scm:git:ssh://git@github.com/yuanjingtech/boot-app-kmp.git"
+                }
+            }
+        }
+    }
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,10 +4,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
-    id("com.vanniktech.maven.publish") version "0.34.0"
+    alias(libs.plugins.publish)
 }
-group = "com.yuanjingtech.boot.app.kmp"
-version = "0.0.1"
 kotlin {
     androidTarget {
         compilerOptions {
@@ -49,40 +47,5 @@ android {
     }
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-}
-// <module directory>/build.gradle.kts
-
-mavenPublishing {
-    publishToMavenCentral()
-
-    signAllPublications()
-
-    coordinates(group.toString(), "shared", version.toString())
-
-    pom {
-        name = "yuanjingtech's boot library for kmp app"
-        description = "yuanjingtech's boot library for kmp app\"."
-        inceptionYear = "2025"
-        url = "https://github.com/yuanjingtech/boot-app-kmp"
-        licenses {
-            license {
-                name = "MIT License"
-                url = "https://github.com/yuanjingtech/boot-app-kmp/blob/main/LICENSE"
-                distribution = "https://github.com/yuanjingtech/boot-app-kmp/blob/main/LICENSE"
-            }
-        }
-        developers {
-            developer {
-                id = "yuanjingtech"
-                name = "yuanjingtech"
-                url = "https://github.com/yuanjingtech"
-            }
-        }
-        scm {
-            url = "https://github.com/yuanjingtech/boot-app-kmp"
-            connection = "scm:git:git://github.com/yuanjingtech/boot-app-kmp.git"
-            developerConnection = "scm:git:ssh://git@github.com/yuanjingtech/boot-app-kmp.git"
-        }
     }
 }
