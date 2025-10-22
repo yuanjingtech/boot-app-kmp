f54b991533dfa2d17fd8849709b1ff168319d877
lotosbin <lotosbin@gmail.com>
2025-10-22 14:55:59 +0800

    build: config publish

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,29 +4,31 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
-
+group = "com.yuanjingtech.boot.app.kmp"
+version = "0.0.1"
 kotlin {
     androidTarget {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     iosArm64()
     iosSimulatorArm64()
-    
+
     jvm()
-    
+
     js {
         browser()
     }
-    
+
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         browser()
     }
-    
+
     sourceSets {
         commonMain.dependencies {
             // put your Multiplatform dependencies here
@@ -46,5 +48,40 @@ android {
     }
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+// <module directory>/build.gradle.kts
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), "shared", version.toString())
+
+    pom {
+        name = "yuanjintech's boot library for kmp app"
+        description = "yuanjintech's boot library for kmp app\"."
+        inceptionYear = "2025"
+        url = "https://github.com/yuanjingtech/boot-app-kmp"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://github.com/yuanjingtech/boot-app-kmp/blob/main/LICENSE"
+                distribution = "https://github.com/yuanjingtech/boot-app-kmp/blob/main/LICENSE"
+            }
+        }
+        developers {
+            developer {
+                id = "yuanjingtech"
+                name = "yuanjingtech"
+                url = "https://github.com/yuanjingtech"
+            }
+        }
+        scm {
+            url = "https://github.com/yuanjingtech/boot-app-kmp"
+            connection = "scm:git:git://github.com/yuanjingtech/boot-app-kmp.git"
+            developerConnection = "scm:git:ssh://git@github.com/yuanjingtech/boot-app-kmp.git"
+        }
     }
 }
