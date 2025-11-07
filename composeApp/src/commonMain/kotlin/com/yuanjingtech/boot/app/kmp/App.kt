e07a0d7a6c5c5bfc85bf09ee8e92018f6aa4c180
lotosbin <lotosbin@gmail.com>
2025-11-07 17:09:05 +0800

    fix: network module issue

--- composeApp/src/commonMain/kotlin/com/yuanjingtech/boot/app/kmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/yuanjingtech/boot/app/kmp/App.kt
@@ -2,27 +2,17 @@ package com.yuanjingtech.boot.app.kmp
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import com.yuanjingtech.boot.app.kmp.demo.demoModule
 import com.yuanjingtech.boot.app.kmp.demo2.demo2Module
 import com.yuanjingtech.boot.app.kmp.di.bootModule
-import com.yuanjingtech.boot.app.kmp.network.NetworkService
-import com.yuanjingtech.boot.app.kmp.network.body
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.koinInject
 import org.koin.dsl.KoinConfiguration
 
 
 @Composable
 @Preview
 fun App() {
-    val networkService = koinInject<NetworkService>()
-    LaunchedEffect("key") {
-        val result = networkService.get({
-            url("http://www.baidu.com")
-        }).body<List<String>>()
-    }
     BootApp(config = KoinConfiguration {
         printLogger()
         modules(bootModule, demoModule, demo2Module)
--- composeApp/src/commonMain/kotlin/com/yuanjingtech/boot/app/kmp/Content.kt
+++ composeApp/src/commonMain/kotlin/com/yuanjingtech/boot/app/kmp/Content.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import com.yuanjingtech.boot.app.kmp.network.NetworkService
+import com.yuanjingtech.boot.app.kmp.network.body
 import com.yuanjingtech.boot.app.kmp.subapp.SubAppManager
 import org.koin.compose.koinInject
 
@@ -16,6 +19,16 @@ fun Content(
     modifier: Modifier = Modifier.Companion,
     subAppManager: SubAppManager = koinInject()
 ) {
+    val networkService = koinInject<NetworkService>()
+    LaunchedEffect("key") {
+        try {
+            val result = networkService.get({
+                url("https://www.baidu.com")
+            }).body<List<String>>()
+        } catch (e: Exception) {
+            println(e.message)
+        }
+    }
     val apps by subAppManager.apps.collectAsState(emptyList())
     Column(
         modifier.verticalScroll(rememberScrollState())
