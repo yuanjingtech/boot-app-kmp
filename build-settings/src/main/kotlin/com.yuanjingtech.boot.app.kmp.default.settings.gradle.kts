0b7c568818c0208466cad0f236d114b9ef0fa7d0
lotosbin <lotosbin@gmail.com>
2026-04-21 22:26:00 +0800

    build(build-logic): clean

--- build-settings/src/main/kotlin/com.yuanjingtech.boot.app.kmp.default.settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2024 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
- */
-
-plugins {
-    id("com.yuanjingtech.boot.app.kmp.repositories")
-    id("com.yuanjingtech.boot.app.kmp.gradle")
-}
