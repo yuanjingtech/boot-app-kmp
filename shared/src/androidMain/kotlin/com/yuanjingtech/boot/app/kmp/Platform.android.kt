1f3779d176c414fe9783183f707120e530294080
lotosbin <lotosbin@gmail.com>
2026-05-16 10:27:07 +0800

    fix: room3

--- shared/src/androidMain/kotlin/com/yuanjingtech/boot/app/kmp/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/yuanjingtech/boot/app/kmp/Platform.android.kt
@@ -1,9 +1,10 @@
 package com.yuanjingtech.boot.app.kmp
 
+import android.content.Context
 import android.os.Build
+import androidx.room3.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.yuanjingtech.boot.app.kmp.data.room3.BootDatabase
-import com.yuanjingtech.boot.app.kmp.data.room3.getDatabaseBuilder
 import kotlinx.coroutines.Dispatchers
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -16,7 +17,10 @@ actual fun getPlatform(): Platform = AndroidPlatform()
 
 actual val bootPlatformModule: Module = module {
     single<BootDatabase> {
-        getDatabaseBuilder(get())
+        val appContext = get<Context>().applicationContext
+        val dbFile = appContext.getDatabasePath("boot_database.db")
+        Room
+            .databaseBuilder<BootDatabase>(context = appContext, name = dbFile.absolutePath)
             .setDriver(BundledSQLiteDriver())
             .setQueryCoroutineContext(Dispatchers.IO)
             .build()
--- shared/src/androidMain/kotlin/com/yuanjingtech/boot/app/kmp/data/room3/DatabaseBuilder.android.kt
+++ shared/src/androidMain/kotlin/com/yuanjingtech/boot/app/kmp/data/room3/DatabaseBuilder.android.kt
@@ -4,13 +4,3 @@ import android.content.Context
 import androidx.room3.Room
 import androidx.room3.RoomDatabase
 
-internal fun getDatabaseBuilder(ctx: Context): RoomDatabase.Builder<BootDatabase> {
-    val appContext = ctx.applicationContext
-    val dbFile = appContext.getDatabasePath("boot_database.db")
-    return Room
-        .databaseBuilder<BootDatabase>(
-            context = appContext,
-            name = dbFile.absolutePath
-        )
-
-}
